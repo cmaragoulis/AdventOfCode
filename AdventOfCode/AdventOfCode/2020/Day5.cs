ec0d0cdadd78830ce453ff9aae6bc169ea8261fe
Chrisostomos Maragoulis <cmaragoulis@protonmail.com>
2020-12-05 09:11:26 +0200

    Add 2020 Day 5 solutions

---
+++ AdventOfCode/AdventOfCode/2020/Day5.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public enum Section
+    {
+        Row, Column
+    }
+
+    public class Day5
+    {
+        private static readonly string inputPath = @"C:\Projects\Advent of Code\AdventOfCode\AdventOfCode\2020\Inputs\Day5.txt";
+
+        public static int Problem1()
+        {
+            var boardingPasses = File.ReadAllLines(inputPath);
+            int maxSeatId = int.MinValue;
+
+            foreach (var boardingPass in boardingPasses)
+            {
+                int seatId = DecodeBoardingPass(boardingPass);
+
+                if (seatId > maxSeatId)
+                {
+                    maxSeatId = seatId;
+                }
+            }
+
+            return maxSeatId;
+        }
+
+        public static int Problem2()
+        {
+            var boardingPasses = File.ReadAllLines(inputPath);
+
+            var seatIds = boardingPasses
+                .Select(boardingPass => DecodeBoardingPass(boardingPass))
+                .OrderBy(seatId => seatId)
+                .ToList();
+
+            for (int i = 0; i < seatIds.Count - 1; i++)
+            {
+                if (seatIds[i + 1] != seatIds[i] + 1)
+                {
+                    return seatIds[i] + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int DecodeBoardingPass(string boardingPass)
+        {
+            int row = DecodeSection(boardingPass, Section.Row);
+            int column = DecodeSection(boardingPass, Section.Column);
+            return (row * 8) + column;
+        }
+
+        private static int DecodeSection(string boardingPass, Section section)
+        {
+            int minValue = 0, maxValue;
+            char lowLetter, highLetter;
+            string boardigPassSection;
+
+            switch (section)
+            {
+                case Section.Row:
+                    maxValue = 127;
+                    boardigPassSection = boardingPass[..7];
+                    lowLetter = 'F';
+                    highLetter = 'B';
+                    break;
+                case Section.Column:
+                    maxValue = 7;
+                    boardigPassSection = boardingPass[^3..];
+                    lowLetter = 'L';
+                    highLetter = 'R';
+                    break;
+                default:
+                    throw new ArgumentException("Invalid section");
+            }
+
+            foreach (var letter in boardigPassSection)
+            {
+                if (letter == lowLetter)
+                {
+                    maxValue -= (int)Math.Round((maxValue - minValue) / 2.0);
+                }
+                else if (letter == highLetter)
+                {
+                    minValue += (int)Math.Round((maxValue - minValue) / 2.0);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid boarding pass");
+                }
+            }
+
+            return boardigPassSection[^1] == lowLetter ? minValue : maxValue;
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2020/Day5Tests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode2020;
+using Xunit;
+
+namespace AdventOfCodeTests2020
+{
+    public class Day5Tests
+    {
+        [Theory]
+        [InlineData("FBFBBFFRLR", 357)]
+        [InlineData("BFFFBBFRRR", 567)]
+        [InlineData("FFFBBBFRRR", 119)]
+        [InlineData("BBFFBBFRLL", 820)]
+        public void DecodeBoardingPass_Correctly_DecodesSeatId(string boardingPass, int expectedSeatId)
+        {
+            //Arrange
+
+            //Act
+            var actualSeatId = Day5.DecodeBoardingPass(boardingPass);
+
+            //Assert
+            Assert.Equal(expectedSeatId, actualSeatId);
+        }
+    }
+}
