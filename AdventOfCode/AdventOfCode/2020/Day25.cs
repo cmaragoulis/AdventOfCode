8fc68668643cf1b9865cd48ec7d46a8fdc1ab998
Chrisostomos Maragoulis <cmaragoulis@protonmail.com>
2020-12-27 11:08:29 +0200

    Add 2020 Day 25 solution

---
+++ AdventOfCode/AdventOfCode/2020/Day25.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+namespace AdventOfCode2020
+{
+    public class Day25
+    {
+        private static readonly string inputPath = @"C:\Projects\Advent of Code\AdventOfCode\AdventOfCode\2020\Inputs\Day25.txt";
+
+        public static long Problem1()
+        {
+            var input = File.ReadAllLines(inputPath);
+
+            var cardPublicKey = int.Parse(input[0]);
+            var doorPublicKey = int.Parse(input[1]);
+
+            var doorLoopSize = DetermineLoopSize(7, doorPublicKey);
+
+            var encryptionKey = TransformSubject(cardPublicKey, doorLoopSize);
+
+            return encryptionKey;
+        }
+
+        public static long TransformSubject(int subject, long loopSize)
+        {
+            var divider = 20201227;
+            long value = 1;
+
+            for (long i = 0; i < loopSize; i++)
+            {
+                value *= subject;
+                value %= divider;
+            }
+
+            return value;
+        }
+
+        public static long DetermineLoopSize(int subject, int publicKey)
+        {
+            long loopSize = 0;
+            var value = 1;
+
+            while (value != publicKey)
+            {
+                loopSize++;
+                value *= subject;
+                value %= 20201227;
+            }
+
+            return loopSize;
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2020/Day25Tests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2020;
+using Xunit;
+
+namespace AdventOfCodeTests2020
+{
+    public class Day25Tests
+    {
+        [Theory]
+        [InlineData(7, 5764801, 8)]
+        [InlineData(7, 17807724, 11)]
+        public static void DetermineLoopSize_Returns_CorrectResult(
+            int subject, int publicKey, long expectedLoopSize)
+        {
+            //Arrange
+
+            //Act
+            var actualLoopSize = Day25.DetermineLoopSize(subject, publicKey);
+
+            //Assert
+            Assert.Equal(expectedLoopSize, actualLoopSize);
+        }
+    }
+}
